e110b33ed514830d0a5df7f50043554a1ec90fd4
陈泽鹏 <zepeng.chen@77hub.com>
2023-08-06 14:31:21 +0800

    修改党组织中心路由，添加gitignore文件

--- src/views/party/fee/index.vue
+++ src/views/party/fee/index.vue
@@ -1,47 +1,266 @@
 <template>
   <div class="container">
-    <Breadcrumb :items="['menu.result', 'menu.result.error']" />
-    <div class="wrapper">
-      <a-result
-        class="result"
-        status="error"
-        :title="$t('error.result.title')"
-        :subtitle="$t('error.result.subTitle')"
+    <a-card class="general-card" :title="$t('menu.party.fee.manage')">
+      <a-table
+        row-key="id"
+        :loading="loading"
+        :row-selection="(rowSelection as TableRowSelection)"
+        :pagination="pagination"
+        :columns="(cloneColumns as TableColumnData[])"
+        :data="renderData"
+        :bordered="false"
+        :size="size"
+        @page-change="onPageChange"
       >
-        <template #extra>
-          <a-space class="operation-wrap" :size="16">
-            <a-button key="again" type="secondary">
-              {{ $t('error.result.goBack') }}
-            </a-button>
-            <a-button key="back" type="primary">
-              {{ $t('error.result.retry') }}
-            </a-button>
-          </a-space>
-        </template>
-      </a-result>
-
-      <div class="details-wrapper">
-        <a-typography-title :heading="6" style="margin-top: 0">
-          {{ $t('error.detailTitle') }}
-        </a-typography-title>
-        <a-typography-paragraph style="margin-bottom: 0">
-          <ol>
-            <li>
-              {{ $t('error.detailLine.record') }}
-              <a-link>
-                <IconLink />
-                {{ $t('error.detailLine.record.link') }}
-              </a-link>
-            </li>
-            <li>{{ $t('error.detailLine.auth') }}</li>
-          </ol>
-        </a-typography-paragraph>
-      </div>
-    </div>
+      </a-table>
+    </a-card>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { computed, ref, reactive, watch, nextTick } from 'vue';
+  import { useI18n } from 'vue-i18n';
+  import useLoading from '@/hooks/loading';
+  import { queryOrgList, PolicyRecord, PolicyParams } from '@/api/manage';
+  import { Pagination } from '@/types/global';
+  import type { SelectOptionData } from '@arco-design/web-vue/es/select/interface';
+  import type {
+    TableColumnData,
+    TableRowSelection,
+  } from '@arco-design/web-vue/es/table/interface';
+  import cloneDeep from 'lodash/cloneDeep';
+  import Sortable from 'sortablejs';
+
+  type SizeProps = 'mini' | 'small' | 'medium' | 'large';
+  type Column = TableColumnData & { checked?: true };
+
+  const rowSelection = reactive({
+    type: 'checkbox',
+    showCheckedAll: true,
+    onlyCurrent: false,
+  });
+  const generateFormModel = () => {
+    return {
+      number: '',
+      name: '',
+      contentType: '',
+      filterType: '',
+      createdTime: [],
+      status: '',
+    };
+  };
+  const { loading, setLoading } = useLoading(true);
+  const { t } = useI18n();
+  const renderData = ref<PolicyRecord[]>([]);
+  const formModel = ref(generateFormModel());
+  const cloneColumns = ref<Column[]>([]);
+  const showColumns = ref<Column[]>([]);
+
+  const size = ref<SizeProps>('medium');
+
+  const basePagination: Pagination = {
+    current: 1,
+    pageSize: 20,
+  };
+  const pagination = reactive({
+    ...basePagination,
+  });
+  const densityList = computed(() => [
+    {
+      name: t('searchTable.size.mini'),
+      value: 'mini',
+    },
+    {
+      name: t('searchTable.size.small'),
+      value: 'small',
+    },
+    {
+      name: t('searchTable.size.medium'),
+      value: 'medium',
+    },
+    {
+      name: t('searchTable.size.large'),
+      value: 'large',
+    },
+  ]);
+  const columns = computed<TableColumnData[]>(() => [
+    {
+      title: t('manage.member.info.query.org'),
+      dataIndex: 'index',
+      slotName: 'index',
+    },
+    {
+      title: t('manage.member.info.query.org'),
+      dataIndex: 'name',
+      slotName: 'name',
+    },
+    {
+      title: t('manage.member.info.query.bus'),
+      dataIndex: 'number',
+    },
+    {
+      title: t('searchTable.columns.name'),
+      dataIndex: 'name',
+    },
+    {
+      title: t('searchTable.columns.contentType'),
+      dataIndex: 'contentType',
+      slotName: 'contentType',
+    },
+    {
+      title: t('searchTable.columns.filterType'),
+      dataIndex: 'filterType',
+    },
+    {
+      title: t('searchTable.columns.count'),
+      dataIndex: 'count',
+    },
+    {
+      title: t('searchTable.columns.createdTime'),
+      dataIndex: 'createdTime',
+    },
+    {
+      title: t('searchTable.columns.status'),
+      dataIndex: 'status',
+      slotName: 'status',
+    },
+    {
+      title: t('searchTable.columns.operations'),
+      dataIndex: 'operations',
+      slotName: 'operations',
+    },
+  ]);
+  const contentTypeOptions = computed<SelectOptionData[]>(() => [
+    {
+      label: t('searchTable.form.contentType.img'),
+      value: 'img',
+    },
+    {
+      label: t('searchTable.form.contentType.horizontalVideo'),
+      value: 'horizontalVideo',
+    },
+    {
+      label: t('searchTable.form.contentType.verticalVideo'),
+      value: 'verticalVideo',
+    },
+  ]);
+  const filterTypeOptions = computed<SelectOptionData[]>(() => [
+    {
+      label: t('searchTable.form.filterType.artificial'),
+      value: 'artificial',
+    },
+    {
+      label: t('searchTable.form.filterType.rules'),
+      value: 'rules',
+    },
+  ]);
+  const statusOptions = computed<SelectOptionData[]>(() => [
+    {
+      label: t('searchTable.form.status.online'),
+      value: 'online',
+    },
+    {
+      label: t('searchTable.form.status.offline'),
+      value: 'offline',
+    },
+  ]);
+  const fetchData = async (
+    params: PolicyParams = { current: 1, pageSize: 20 }
+  ) => {
+    setLoading(true);
+    try {
+      const { data } = await queryOrgList(params);
+      renderData.value = data;
+      // pagination.current = params.current;
+      // pagination.total = data.total;
+    } catch (err) {
+      // you can report use errorHandler or other
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const search = () => {
+    fetchData({
+      ...basePagination,
+      ...formModel.value,
+    } as unknown as PolicyParams);
+  };
+  const onPageChange = (current: number) => {
+    fetchData({ ...basePagination, current });
+  };
+
+  fetchData();
+  const reset = () => {
+    formModel.value = generateFormModel();
+  };
+
+  const handleSelectDensity = (
+    val: string | number | Record<string, any> | undefined,
+    e: Event
+  ) => {
+    size.value = val as SizeProps;
+  };
+
+  const handleChange = (
+    checked: boolean | (string | boolean | number)[],
+    column: Column,
+    index: number
+  ) => {
+    if (!checked) {
+      cloneColumns.value = showColumns.value.filter(
+        (item) => item.dataIndex !== column.dataIndex
+      );
+    } else {
+      cloneColumns.value.splice(index, 0, column);
+    }
+  };
+
+  const exchangeArray = <T extends Array<any>>(
+    array: T,
+    beforeIdx: number,
+    newIdx: number,
+    isDeep = false
+  ): T => {
+    const newArray = isDeep ? cloneDeep(array) : array;
+    if (beforeIdx > -1 && newIdx > -1) {
+      // 先替换后面的，然后拿到替换的结果替换前面的
+      newArray.splice(
+        beforeIdx,
+        1,
+        newArray.splice(newIdx, 1, newArray[beforeIdx]).pop()
+      );
+    }
+    return newArray;
+  };
+
+  const popupVisibleChange = (val: boolean) => {
+    if (val) {
+      nextTick(() => {
+        const el = document.getElementById('tableSetting') as HTMLElement;
+        const sortable = new Sortable(el, {
+          onEnd(e: any) {
+            const { oldIndex, newIndex } = e;
+            exchangeArray(cloneColumns.value, oldIndex, newIndex);
+            exchangeArray(showColumns.value, oldIndex, newIndex);
+          },
+        });
+      });
+    }
+  };
+
+  watch(
+    () => columns.value,
+    (val) => {
+      cloneColumns.value = cloneDeep(val);
+      cloneColumns.value.forEach((item, index) => {
+        item.checked = true;
+      });
+      showColumns.value = cloneDeep(cloneColumns.value);
+    },
+    { deep: true, immediate: true }
+  );
+</script>
 
 <script lang="ts">
   export default {
@@ -51,7 +270,7 @@
 
 <style scoped lang="less">
   .container {
-    padding: 0 20px 20px 20px;
+    padding:20px;
   }
   .wrapper {
     padding: 24px 150px;
--- src/views/party/locale/zh-CN.ts
+++ src/views/party/locale/zh-CN.ts
@@ -3,4 +3,7 @@ export default {
   'menu.party.fee': '党费缴纳',
   'menu.party.fee.manage': '缴纳管理',
   'menu.party.fee.wages': '基数管理',
+  'menu.party.activity': '组织活动',
+  'menu.party.activity.shyk': '三会一课',
+  'menu.party.activity.txth': '谈心谈话',
 };
